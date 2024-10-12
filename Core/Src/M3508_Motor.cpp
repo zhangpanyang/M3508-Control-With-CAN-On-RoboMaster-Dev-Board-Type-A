60c591c026288d993e087705a169a24581ef53c2
zhangpanyang <azhangpanyang@126.com>
2024-10-12 17:39:20 +0800

    重构

---
+++ Core/Src/M3508_Motor.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Sunny on 24-10-12.
+//
+
+#include "M3508_Motor.h"
+
+M3508_Motor::M3508_Motor(){
+	ratio_ = 3591.0 / 187;
+	angle_ = 0;
+	delta_angle_ = 0;
+	ecd_angle_ = 0;
+	last_ecd_angle_ = 0;
+	delta_ecd_angle_ = 0;
+	rotate_speed_ = 0;
+	current_ = 0.87;
+	temprature_ = 0;
+}
+
+float linearMapping(int in,int in_min,int in_max,float out_min,float out_max) {
+	return (out_max-out_min)*(float)(in-in_min)/(float)(in_max-in_min)+out_min;
+}
+
+void M3508_Motor::canRxMsgCallback_v2(uint8_t rx_data[8]) {
+	// 转子机械角度
+	int ecd_angle = ((int)rx_data[0] << 8) | rx_data[1];
+	ecd_angle_ = linearMapping(ecd_angle, 0, 8191, 0, 360); // 映射
+
+	delta_ecd_angle_ = ecd_angle_ - last_ecd_angle_;	//计算增量
+
+	if (delta_ecd_angle_ > 180) delta_ecd_angle_ -= 360;
+	if (delta_ecd_angle_ < -180) delta_ecd_angle_ += 360;
+
+	last_ecd_angle_ = ecd_angle_;	//更新
+
+	// 转子转速
+	int rotate_speed = ((int)rx_data[2]<< 8) | rx_data[3];
+	rotate_speed_ = rotate_speed * 6;
+
+	// 实际转矩电流
+	int current = ((int)rx_data[4]<< 8) | rx_data[5]; // 合并
+	current_ = linearMapping(current, -16384, 16384, -20, 20); // 映射
+
+	// 电机温度
+	temprature_ = rx_data[6];
+}
--- Core/Src/can_task.cpp
+++ Core/Src/can_task.cpp
@@ -3,46 +3,7 @@
 //
 
 #include "can_task.h"
-
-M3508_Motor::M3508_Motor(){
-    ratio_ = 3591.0 / 187; 
-    angle_ = 0;
-	delta_angle_ = 0;
-    ecd_angle_ = 0;
-    last_ecd_angle_ = 0;
-    delta_ecd_angle_ = 0; 
-    rotate_speed_ = 0;
-    current_ = 0.87;
-    temprature_ = 0;
-}
-
-float linearMapping(int in,int in_min,int in_max,float out_min,float out_max) {
-    	return (out_max-out_min)*(float)(in-in_min)/(float)(in_max-in_min)+out_min;
-}
-
-void M3508_Motor::canRxMsgCallback_v2(uint8_t rx_data[8]) {
-	// 转子机械角度
-	int ecd_angle = ((int)rx_data[0] << 8) | rx_data[1];
-	ecd_angle_ = linearMapping(ecd_angle, 0, 8191, 0, 360); // 映射
-
-	delta_ecd_angle_ = ecd_angle_ - last_ecd_angle_;	//计算增量
-
-	if (delta_ecd_angle_ > 180) delta_ecd_angle_ -= 360;
-	if (delta_ecd_angle_ < -180) delta_ecd_angle_ += 360;
-
-	last_ecd_angle_ = ecd_angle_;	//更新
-
-	// 转子转速
-	int rotate_speed = ((int)rx_data[2]<< 8) | rx_data[3];
-	rotate_speed_ = rotate_speed * 6;
-
-	// 实际转矩电流
-	int current = ((int)rx_data[4]<< 8) | rx_data[5]; // 合并
-	current_ = linearMapping(current, -16384, 16384, -20, 20); // 映射
-
-	// 电机温度
-	temprature_ = rx_data[6];
-}
+#include "M3508_Motor.h"
 
 void canTaskInit()
 {
@@ -62,6 +23,8 @@ void canTaskInit()
 	HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
 }
 
+M3508_Motor motor;
+
 uint8_t rxData[8];
 CAN_RxHeaderTypeDef rxHeader;
 
@@ -70,5 +33,6 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
 	if (hcan == &hcan1)
 	{
 		HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &rxHeader, rxData);
+		motor.canRxMsgCallback_v2(rxData);
 	}
 }
