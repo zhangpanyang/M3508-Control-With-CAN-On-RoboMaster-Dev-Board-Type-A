84df0b7ee3fc517ec3ce8aa0318b1b496495f6af
zhangpanyang <azhangpanyang@126.com>
2024-10-12 17:26:33 +0800

    抄bcc Motor

--- Core/Src/can_task.h
+++ Core/Src/can_task.h
@@ -5,6 +5,28 @@
 #ifndef CAN_TASK_H
 #define CAN_TASK_H
 
+#include "main.h"
+#include "can.h"
+
 void canTaskInit();
+
+class M3508_Motor
+{
+private:
+
+public:
+	float ratio_; // 电机减速比
+	float angle_; // deg 输出端累计转动角度
+	float delta_angle_; // deg 输出端新转动的角度
+	float ecd_angle_; // deg 当前电机编码器角度
+	float last_ecd_angle_; // deg 上次电机编码器角度
+	float delta_ecd_angle_; // deg 编码器端新转动的角度
+	float rotate_speed_; // dps 反馈转子转速
+	float current_;
+	float temprature_;
+
+	M3508_Motor();
+	void canRxMsgCallback_v2(uint8_t rx_data[8]);
+};
 
 #endif //CAN_TASK_H
