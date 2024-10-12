782a9a0874ca34590f743d4a9747adb310dda05c
zhangpanyang <azhangpanyang@126.com>
2024-10-12 17:47:26 +0800

    能够读取所有的反馈量

--- MDK-ARM/STM32CANTest/m3508_motor.d
+++ MDK-ARM/STM32CANTest/m3508_motor.d
@@ -1,5 +1,6 @@
-stm32cantest/can_task.o: ..\Core\Src\can_task.cpp ..\Core\Src\can_task.h \
-  ..\Core\Inc\main.h ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
+stm32cantest/m3508_motor.o: ..\Core\Src\M3508_Motor.cpp \
+  ..\Core\Src\M3508_Motor.h ..\Core\Inc\main.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
   ..\Core\Inc\stm32f4xx_hal_conf.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_def.h \
@@ -31,5 +32,4 @@ stm32cantest/can_task.o: ..\Core\Src\can_task.cpp ..\Core\Src\can_task.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_flash_ex.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_flash_ramfunc.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr.h \
-  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h \
-  ..\Core\Inc\can.h ..\Core\Src\M3508_Motor.h
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h
