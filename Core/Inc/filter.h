0cd6c7b44d0c92099972acdc26d95c45ac68aa95
zhangpanyang <azhangpanyang@126.com>
2024-10-26 19:39:54 +0800

    实现filter

--- Core/Inc/filter.h
+++ Core/Inc/filter.h
@@ -5,4 +5,20 @@
 #ifndef FILTER_H
 #define FILTER_H
 
+#include "main.h"
+#include <queue>
+
+class FilterMovingAverage
+{
+private:
+	std::queue<float> valueQueue_;
+	float sum_;
+public:
+	uint32_t len_;
+
+	FilterMovingAverage(uint32_t len);
+	void push(float value);
+	float getResult();
+};
+
 #endif //FILTER_H
--- Core/Src/filter.cpp
+++ Core/Src/filter.cpp
@@ -1,3 +1,29 @@
 //
 // Created by Sunny on 24-10-26.
 //
+
+#include "filter.h"
+
+FilterMovingAverage::FilterMovingAverage(uint32_t len)
+{
+	len_ = len;
+	sum_ = 0;
+}
+
+void FilterMovingAverage::push(float value)
+{
+	if (valueQueue_.size() == len_)
+	{
+		sum_ -= valueQueue_.front();
+		valueQueue_.pop();
+	}
+	valueQueue_.push(value);
+	sum_ += value;
+}
+
+float FilterMovingAverage::getResult()
+{
+	if (valueQueue_.empty())
+		return 0;
+	return sum_ / (float)valueQueue_.size();
+}
